bf805d04244b4a06f1410e6a1a95ac3e7e58e638
Ahmed Javed <88615893+vitiligo610@users.noreply.github.com>
2025-11-17 20:23:29 +0500

    add rest controller for fetching sensor readings
    Signed-off-by: Ahmed Javed <88615893+vitiligo610@users.noreply.github.com>

--- src/main/java/com/aqi/repository/SensorNodeRepository.java
+++ src/main/java/com/aqi/repository/SensorNodeRepository.java
@@ -1,8 +1,10 @@
 package com.aqi.repository;
 
 import com.aqi.entity.SensorNode;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SensorNodeRepository extends JpaRepository<SensorNode, String> {
